e32fca26c1157052e90a477ea4e6b99f3d93e87d
mwibutsa <mflohost@gmail.com>
2018-12-16 17:31:54 +0200

    I have added the ability to save users in an array as they register

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -1,3 +1,27 @@
-function showNavigation(){
+class User{
+    constructor(firstname,lastname,otheName,email,username,phoneNumber,password){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.otheName = otheName;
+        this.email = email;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+}
+var users = [];
 
+function registerUser(form){
+    var form = document.getElementById('signup-form');
+    var firstname = form.firstname.value;
+    var lastname = form.lastname.value;
+    var otheName = form.otherName.value;
+    var email = form.email.value;
+    var username = form.username.value;
+    var phoneNumber = form.phoneNumber.value;
+    var password = form.password.value;
+    var cpassword = form.cpassword.value;
+    users.push(new User(firstname,lastname,otheName,email,username,phoneNumber,password))
+    console.log(users);
+    return false;
 }
