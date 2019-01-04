12a0e798a305761cb667be06029494cbb6a870ed
mwibutsa <mflohost@gmail.com>
2019-01-04 23:17:47 +0200

    creating get meetup by id controller

---
+++ server/controllers/specific-meetup.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import meetups from '../models/meetups';
+const getMeetupById = (req, res) => {
+  const meetupById = meetups.find(meetup => parseInt(meetup.id) === parseInt(req.params.id));
+  if (meetupById) {
+    res.json(meetupById);
+  } else {
+    res.json({
+      status: 404,
+      error: 'The meetup with given id is not found',
+    });
+  }
+};
+export default getMeetupById;
