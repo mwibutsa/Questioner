360a4c85a91e93b312de6510c583200b7d1bd41d
mwibutsa <mflohost@gmail.com>
2019-01-05 01:41:19 +0200

    specifying node version

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ import meetups from './server/routes/meetups';
 import questions from './server/routes/questions';
 import reservations from './server/routes/reservations';
 
-const port  = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
-app.set('port',port);
-app.listen(app.get('port'),()=>{
-    console.log(`server started on port ${port}`);
-    
+app.set('port', port);
+app.listen(app.get('port'), () => {
+  console.log(`server started on port ${port}`);
 });
-
