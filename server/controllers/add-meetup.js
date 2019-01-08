d4000548fa974cf2fe5d04c847b8df8588890575
mwibutsa <mflohost@gmail.com>
2019-01-09 01:18:38 +0200

    eslint removing unwanted imports

--- server/controllers/add-meetup.js
+++ server/controllers/add-meetup.js
@@ -1,25 +1,26 @@
-import fs from 'fs';
-import path from 'path';
 import uuid from 'uuid';
 import meetups from '../models/meetup';
 import Database from '../database/db_connection';
+
 let filePath = '';
 
-const addMeetup = async (req, res) =>{
-  if(req.files){
+const addMeetup = async (req, res) => {
+  if (req.files) {
     const meetupImages = req.files.images;
-    filePath = '../../meetup-files/'+meetupImages.name;
-    meetupImages.mv(filePath,(error)=> {
-      if(error) res.json({
-        status:500,
-        error:error
-      });
+    filePath = `../../meetup-files/${meetupImages.name}`;
+    meetupImages.mv(filePath, (error) => {
+      if (error) {
+        res.json({
+          status: 500,
+          error,
+        });
+      }
     });
   }
-  let meetupQuery = `INSERT INTO meetup_table (id,created_on,location,topic,happening_on) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
-  let { location,topic,happeningOn } = req.body
-  let newMeetup =[uuid.v4(),new Date(),location,topic,happeningOn];
-  const { rows } = await Database.executeQuery(meetupQuery,newMeetup);
-  res.json({status:200,data:meetups});
+  const meetupQuery = 'INSERT INTO meetup_table (id,created_on,location,topic,happening_on) VALUES ($1,$2,$3,$4,$5) RETURNING *;';
+  const { location, topic, happeningOn } = req.body;
+  const newMeetup = [uuid.v4(), new Date(), location, topic, happeningOn];
+  await Database.executeQuery(meetupQuery, newMeetup);
+  res.json({ status: 200, data: meetups });
 };
 export default addMeetup;
