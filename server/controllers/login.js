fb592445ed5a453e01bd791f8379de0472f0f217
mwibutsa <mflohost@gmail.com>
2019-01-09 23:54:02 +0200

    importing joi, helper and validator

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,10 +1,14 @@
 import joi from 'joi';
 import users from '../models/user';
+import Helper from '../helpers/helper';
+import validate from '../helpers/validation';
+
 const authenticateUser = (req,res) => {
     let userAccount = {
         username:req.body.username,
         password:req.body.password
     };
+    joi.validate(userAccount,)
     const user = users.find(user => user.username === userAccount.username && user.password === userAccount.password);
     if(user){
         res.json({
