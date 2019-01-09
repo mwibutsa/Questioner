bda43ed761fb24fe76c18a8c3f7dc89cb5f9e1fa
mwibutsa <mflohost@gmail.com>
2019-01-09 12:28:41 +0200

    linting with eslint

--- server/controllers/upcomming-meetups.js
+++ server/controllers/upcomming-meetups.js
@@ -1,28 +1,26 @@
 import meetups from '../models/meetup';
 
 // get all meetups upcomming meetups
-
+/**
+ *
+ * @param {mList} meetup list
+ * @param {upC} res upcomming meetups
+ */
+const isUpcomming = meetup => (new Date(meetup.happening_on)).getTime() > (new Date()).getTime();
 const getUpcommingMeetups = async (req, res) => {
   // get al meetups from the database
-  const allMettups = await meetups();
+  const mList = await meetups();
   // filter meeups with happening date greater than todya
-  const upComming = allMettups.filter(meetup =>{
-    let happeningOn = (new Date(meetup.happening_on)).getTime();
-    if(happeningOn > (new Date()).getTime()){
-      return meetup;
-    }
+  const upC = mList.filter(meetup => isUpcomming(meetup));
 
-  });
-  
   // if there are upcomming meetups
 
-  if (upComming) {
-    res.json({status:200,data:upComming});
-  } else { // if there is no upcomming meetup
-    res.json({
-      status: 404,
-      error: 'No upcomming meetups',
-    });
-  }
+  if (upC) {
+    return res.json({ status: 200, data: upC });
+  } // if there is no upcomming meetup
+  return res.json({
+    status: 404,
+    error: 'No upcomming meetups',
+  });
 };
 export default getUpcommingMeetups;
