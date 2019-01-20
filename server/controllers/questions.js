791c7f84e5968cdc6e7a445a75046bff9f6ce08a
mwibutsa <mflohost@gmail.com>
2019-01-20 20:53:22 +0200

    Chore #163343380 Get questions from database

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -1,18 +1,25 @@
-import questions from '../models/question';
+import Database from '../db/db-connection';
 import Helper from '../helpers/helpers';
 
-const getQuestions = (req, res) => {
-  const questionsOnMeetup = questions.find(question => Helper.intCastCompare(req.params.id, question.meetup));
-  if (questionsOnMeetup) {
-    return res.json({
-      status: 200,
-      data: questionsOnMeetup,
+const getQuestions = async (req, res) => {
+  const sql = `SELECT * FROM question_table WHERE meetup = '${req.params.id}'`;
+  try {
+    const { rows } = await Database.executeQuery(sql);
+    if (rows) {
+      return res.status(200).json({
+        status: 200,
+        data: rows,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error,
     });
   }
-
-  return res.json({
-    status: 400,
-    error: 'No questions are found on This meetup',
+  return res.status(404).json({
+    status: 404,
+    error: 'No Questions are availbale for this meetup',
   });
 };
 
