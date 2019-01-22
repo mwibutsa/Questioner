6a612ad8ceb149793150a429aa2aeba52b3c4903
mwibutsa <mflohost@gmail.com>
2019-01-22 00:20:37 -0800

    [chore #163343380] Persisting new meetups with database

--- server/controllers/add-meetup.js
+++ server/controllers/add-meetup.js
@@ -20,20 +20,26 @@ const addMeetup = (req, res) => {
       result.happeningOn,
     ];
     const sql = 'INSERT INTO meetup_table(id, created_on,location,topic,happening_on) VALUES ($1,$2,$3,$4,$5) RETURNING *';
-    try {
-      const { rows } = await Database.executeQuery(sql, newMeetup);
-      if (rows) {
-        res.status(201).json({
-          status: 201,
-          data: rows,
+    const meetup = Database.executeQuery(sql);
+    meetup.then((result) => {
+      if(result.rows.length) {
+        return res.status(200).json({
+          status:200,
+          data:result.rows,
         });
       }
-    } catch (error) {
-      res.status(500).json({
-        status: 500,
-        error,
+      else{
+        return res.status(400).json({
+          status: 400,
+          error: ' Failled To save data in the database'
+        });
+      }
+        });
+    }).catch((error) => {
+      res.json.status(500).json({
+        status:500,
+        error:'Internal server error',
       });
-    }
-  });
+    });
 };
 export default addMeetup;
