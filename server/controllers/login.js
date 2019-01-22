17a70b2b63052814f41e38559bc01ea35e4e905f
mwibutsa <mflohost@gmail.com>
2019-01-22 05:43:52 -0800

    Sign a json web token

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -8,9 +8,9 @@ const authenticateUser = async (req, res) => {
   joi.validate(req.body, Validation.loginSchema, Validation.validationOption,
     async (err, result) => {
       if (err) {
-        return res.status(500).json({
-          status: 500,
-          error: err,
+        return res.status(400).json({
+          status: 400,
+          error: err.details,
         });
       }
       const userAccount = {
@@ -20,12 +20,12 @@ const authenticateUser = async (req, res) => {
       const sql = `SELECT * FROM user_table WHERE email = '${userAccount.email}'`;
         const  user = Database.executeQuery(sql);
         user.then((result) => {
-          if (Helper.comparePassword(userAccount.password, result.rows[0].password)) {
-  
-            res.status(202).json({
-              status: 202,
-              data: rows,
-            });
+          if(result.rows.length){
+            if (Helper.comparePassword(userAccount.password, result.rows[0].password)) {
+             const token = jsonWebToken.sign(result.rows,'secret_@1!9(9)6^%',{expiresIn:'24h'});
+             req.token = token;
+              return res.status(202).json({ status: 202, data: result.rows, });
+            }
           }
         })
     });
