cd2c6e0701e4f4952724b39e227f885a1e3f77cc
mwibutsa <mflohost@gmail.com>
2019-02-03 12:16:40 +0200

    Save before fromatting the machine

--- server/controllers/delete-meetup.js
+++ server/controllers/delete-meetup.js
@@ -3,7 +3,7 @@ import Database from '../db/db-connection';
 const deleteMeetup = async (req, res) => {
   const sql = `DELETE FROM meetup_table WHERE id = '${req.params.id}' RETURNING *`;
   Database.executeQuery(sql).then((result) => {
-    res.status(202).json({ status: 202, message: `meetup was deleted ${result.rows}` })
+    res.status(202).json({ status: 202, message: `meetup was deleted ${result.rows}` });
   }).catch(error => res.status(500).json({ status: 500, error: `Server error ${error}` }));
 };
 
--- server/controllers/edit-meetup.js
+++ server/controllers/edit-meetup.js
@@ -3,21 +3,21 @@ import Database from '../db/db-connection';
 import Validation from '../helpers/validation';
 
 const editMeetup = (req, res) => {
-    joi.validate(req.body, Validation.meetupSchema,Validation.validationOption).then((result) => {
-        const { topic, location, happeningOn } = result;
-        const updatedMeetup = [topic,location,happeningOn];
-        const sql = `UPDATE meetup_table SET topic = $1, location = $2, happening_on = $3
+  joi.validate(req.body, Validation.meetupSchema, Validation.validationOption).then((result) => {
+    const { topic, location, happeningOn } = result;
+    const updatedMeetup = [topic, location, happeningOn];
+    const sql = `UPDATE meetup_table SET topic = $1, location = $2, happening_on = $3
          WHERE id = '${req.params.id}' RETURNING *`;
-         Database.executeQuery(sql, updatedMeetup).then((updateResult) => {
-             if(updateResult.rows) {
-                 if(updateResult.rows.length) {
-                     return res.status(202).json({ status: 202, data: updateResult.rows});
-                 }
-             } else {
-                 return res.status(500).json({ status: 500, erro: `Not updated: ${updateResult}`})
-             }
-         })
-           .catch(error => res.status(500).json({ status: 500, error: `Server error ${error}` }));
-    }).catch(error => res.status(400).json({ status: 400, error: error.details }))
+    Database.executeQuery(sql, updatedMeetup).then((updateResult) => {
+      if (updateResult.rows) {
+        if (updateResult.rows.length) {
+          return res.status(202).json({ status: 202, data: updateResult.rows });
+        }
+      } else {
+        return res.status(500).json({ status: 500, erro: `Not updated: ${updateResult}` });
+      }
+    })
+      .catch(error => res.status(500).json({ status: 500, error: `Server error ${error}` }));
+  }).catch(error => res.status(400).json({ status: 400, error: error.details }));
 };
 export default editMeetup;
