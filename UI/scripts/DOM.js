5b3ff6d6a63a9f5f155c562166758e119aec8258
mwibutsa <mwibutsa>
2019-02-04 16:19:35 -0500

    save UI modifications

--- UI/scripts/DOM.js
+++ UI/scripts/DOM.js
@@ -1,1 +1,4 @@
-
+const allMeetups = fetch('https://mwibutsa.github.io/Questioner/UI/meetups.html');
+allMeetups.then(result => result.json()).then((meetups) => {
+  console.log(meetups);
+}).catch(error => console.log(error));
