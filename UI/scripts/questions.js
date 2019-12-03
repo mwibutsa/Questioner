7290d25b3af379de1487325bc7a76f40d4351181
mwibutsa <mflohost@gmail.com>
2019-12-03 19:17:31 +0200

    Refactor Langing page

--- UI/scripts/questions.js
+++
@@ -1,4 +0,0 @@
-const headers = new Headers();
-headers.append('Accept', 'application/json');
-headers.append('Content-type', 'application/json');
-headers.append('Authorization', `Bearer ${(JSON.parse(localStorage.getItem('user-data'))).token}`);
